e01d3863cc7a0a110e02e5477117d9c8d3c0f79f
bollsay <bollsay@bk.ru>
2019-10-16 19:58:49 +0600

    printreversealphabet

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for i := rune(122); i >= rune(97); i-- {
+	for i := 'z'; i >= 'a'; i-- {
 		z01.PrintRune(i)
 	}
-	z01.PrintRune(rune(10))
+	z01.PrintRune(10)
 }
