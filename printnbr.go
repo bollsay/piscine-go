896602f97aa4c653ac7228244565c43d3a34ec6e
bollsay <bollsay@bk.ru>
2019-10-17 23:51:58 +0600

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -1,25 +1,29 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-
-	t := 1
 	if n < 0 {
-		t = -1
-		z01.PrintRune(45)
+		z01.PrintRune('-')
 	}
-	if n != 0 {
-		f := (n / 10) * t
-		if f != 0 {
-			z01.PrintRune(f)
-		}
-		k := (n % 10 * t) + '0'
-		z01.PrintRune(k)
-	} else {
-		z01.PrintRune('0')
-	}
+	SetNbr(n)
+}
 
+func SetNbr(n int) {
+	h := '0'
+	if n == 0 {
+		z01.PrintRune(h)
+		return
+	}
+	for i := 1; i <= n%10; i++ {
+		h++
+	}
+	for i := -1; i <= n%10; i-- {
+		h++
+	}
+	if n/10 != 0 {
+		SetNbr(n / 10)
+	}
+	z01.PrintRune(h)
+	return
 }
