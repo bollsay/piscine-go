06d3f266cc5a1db85e30a5d04711a3887c809c75
bollsay <bollsay@bk.ru>
2019-10-18 00:53:24 +0600

    strrev.go

--- strrev.go
+++ strrev.go
@@ -1,10 +1,14 @@
 package piscine
 
 func StrRev(s string) string {
-
-	var reverse string
-	for i := len(s) - 1; i >= 0; i-- {
-		reverse += string(s[i])
+	r := []rune(s)
+	nc := 0
+	nword := ""
+	for range s {
+		nc = nc + 1
 	}
-	return reverse
+	for i := nc - 1; i >= 0; i-- {
+		nword = nword + string(r[i])
+	}
+	return string(nword)
 }
