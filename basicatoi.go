ff022a7c5032b0035824b02b33a6ef9d50178a57
bollsay <bollsay@bk.ru>
2019-10-18 20:03:41 +0600

    basicatoi.go

--- basicatoi.go
+++ basicatoi.go
@@ -1,19 +1,13 @@
 package piscine
 
 func BasicAtoi(s string) int {
-
-	arrayStr := []rune(s)
-	n := len(s)
-	ans := 0
-	for i := 0; i < n; i++ {
-		if arrayStr[i] < '0' || arrayStr[i] > '9' {
-
-			return ans
-		} else {
-			ans *= 10
-			ans += int(arrayStr[i]) - '0'
+	dec := 0
+	for _, j := range s {
+		ed := 0
+		for i := '1'; i <= j: i++ {
+			ed = ed +1
 		}
-
+		dec = dec*10 + ed
 	}
-	return ans
+	return dec
 }
