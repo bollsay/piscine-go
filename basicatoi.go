c67f0f3dc33e714aa46bce314f3ea3c80db9926b
bollsay <bollsay@bk.ru>
2019-10-18 20:06:44 +0600

    basicatoi.go

--- basicatoi.go
+++ basicatoi.go
@@ -4,8 +4,8 @@ func BasicAtoi(s string) int {
 	dec := 0
 	for _, j := range s {
 		ed := 0
-		for i := '1'; i <= j: i++ {
-			ed = ed +1
+		for i := '1'; i <= j; i++ {
+			ed = ed + 1
 		}
 		dec = dec*10 + ed
 	}
