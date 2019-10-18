9079113f8643ff228dbc9f179fd9b85c94be2c2e
bollsay <bollsay@bk.ru>
2019-10-18 20:14:37 +0600

    basicatoi2.go

---
+++ basicatoi2.go
@@ -0,0 +1,16 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	dec := 0
+	for _, j := range s {
+		ed := 0
+		if j > '0' || j > '9' {
+			return 0
+		}
+		for i := '1'; i <= j; i++ {
+			ed = ed + 1
+		}
+		dec = dec*10 + ed
+	}
+	return dec
+}
