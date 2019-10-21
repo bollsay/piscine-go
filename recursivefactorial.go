c3eb225fe17d46b61be11899746c6552dcf56fab
bollsay <bollsay@bk.ru>
2019-10-21 15:19:51 +0600

    recursivefactorial.go

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,8 +4,11 @@ func RecursiveFactorial(nb int) int {
 	if nb == 0 {
 		return 1
 	}
-	if nb < 1 {
+	if nb < 0 || nb > 50 {
 		return 0
+	}
+	if nb == 1 {
+		return 1
 	}
 	return nb * RecursiveFactorial(nb-1)
 }
