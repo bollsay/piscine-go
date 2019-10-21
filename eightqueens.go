72de3da86237be0c9baad54cb1ea0dd5c98a21cd
bollsay <bollsay@bk.ru>
2019-10-21 16:39:35 +0600

    eightqueens.go

---
+++ eightqueens.go
@@ -0,0 +1,59 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+const n int = 8
+
+func EightQueens() {
+	var rows [n]int
+	getQueens(rows, 0)
+}
+
+func getQueens(rows [n]int, a int) {
+	for i := 0; i < n; i++ {
+		if checkPlace(rows, i, a) {
+			rows[a] = i
+			if a == n-1 {
+				for j := 0; j < n; j++ {
+					z01.PrintRune(getRunesForChess(rows[j]))
+				}
+				z01.PrintRune('\n')
+			} else {
+				getQueens(rows, a+1)
+			}
+		}
+	}
+}
+
+func checkPlace(rows [n]int, a, b int) bool {
+	if b == 0 {
+		return true
+	}
+	for i := 0; i < b; i++ {
+		if rows[i] == a || rows[i] == a+b-i || rows[i] == a-b+i {
+			return false
+		}
+	}
+	return true
+}
+
+func getRunesForChess(ch int) rune {
+	switch ch {
+	case 0:
+		return '1'
+	case 1:
+		return '2'
+	case 2:
+		return '3'
+	case 3:
+		return '4'
+	case 4:
+		return '5'
+	case 5:
+		return '6'
+	case 6:
+		return '7'
+	default:
+		return '8'
+	}
+}
