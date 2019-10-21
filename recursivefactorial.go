352de3cd9bddd1e80378294f440c51a1e504e1c4
bollsay <bollsay@bk.ru>
2019-10-21 15:12:48 +0600

    recursivefactorial.go

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,14 +1,8 @@
-package main
-
-import "fmt"
+package piscine
 
 func RecursiveFactorial(nb int) int {
 	if nb == 0 {
 		return 1
 	}
 	return nb * RecursiveFactorial(nb-1)
-}
-
-func main() {
-	fmt.Println(RecursiveFactorial(4))
 }
