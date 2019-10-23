fdb32021e1c7c79f3068f84dca5db0508a8486ac
bollsay <bollsay@bk.ru>
2019-10-23 14:54:20 +0600

    index.go

--- index.go
+++ index.go
@@ -3,25 +3,13 @@ package piscine
 func Index(s string, toFind string) int {
 	runes := []rune(s)
 	q := []rune(toFind)
-	counter := 0
-	qc := 0
-	for range runes {
-		counter++
-	}
-	for range q {
-		qc++
-	}
 
 	for i, v := range runes {
 		if v == q[0] {
 			found := true
 			for j, qv := range q {
-				if counter-1-i >= qc {
-					if runes[i+j] != qv {
-						found = false
-					}
-				} else {
-					return -1
+				if runes[i+j] != qv {
+					found = false
 				}
 			}
 			if found == true {
